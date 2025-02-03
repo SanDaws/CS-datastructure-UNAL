63be901f9e655d70e07a105892db1a8f4d645fe2
Daws <castrogiraldosantiago@gmail.com>
2025-02-03 11:19:23 -0500

    linked list moved to sem6

---
+++ sem6/Classes/LinkedList/LinkedList.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sem6.Classes.LinkedList;
+    public class LinkedList
+    {
+        public Node Cabeza;
+    public Node Cola;
+
+    public LinkedList(){
+        Cabeza= null;
+        Cola=null;
+    }
+    public void Agregar(object valor)
+    {
+        Node newnode = new Node(valor);
+
+        if (Cabeza == null)
+    {
+
+        Cabeza = newnode;
+        Cola = newnode;
+    }
+    else
+    {
+        Cola.Siguiente =newnode;
+        Cola= newnode;           
+    }
+        
+    }
+    public void 
+    Agregar(Node nodo)
+    {
+        nodo.Siguiente=null;
+
+        if (Cabeza == null)
+        {
+            Cabeza = nodo;
+            Cola= nodo;
+        }
+        else
+        {
+           Cola.Siguiente =nodo;
+            Cola= nodo;   
+        }
+        
+    }
+    public Node eliminarCabezaSoft(){
+        Node node= Cabeza;
+
+        Console.WriteLine();
+        Console.WriteLine(Cabeza==null);
+        Cabeza= Cabeza.Siguiente;
+        
+        return node;
+
+    }
+    }
