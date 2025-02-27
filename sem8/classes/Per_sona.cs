445f6aff5754286b4e636d57eef19dfd4f3e89ea
Daws <castrogiraldosantiago@gmail.com>
2025-02-27 13:17:13 -0500

    problema 2 laboratio 8 classes

--- sem8/classes/Per_sona.cs
+++ sem8/classes/Per_sona.cs
@@ -7,6 +7,22 @@ namespace sem8.classes
 {
     public class Per_sona
     {
-        
+        public string Nombre{get;set;}
+        public int Identificacion{get;set;}
+        public int key{get;set;}
+
+        public Per_sona(string Nombre, string id){
+            this.Nombre=Nombre;
+            Identificacion= int.Parse(id);
+            key=cont(id);
+        }
+        public int cont(string i){
+            int c=0;
+            foreach (string item in i.Split())
+            {
+                c += int.Parse(item);     
+            }
+            return c;
+        }
     }
 }
